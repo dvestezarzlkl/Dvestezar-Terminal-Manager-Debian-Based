2e059372145a16915db63c0dcec9de550bcc7c4a
dvestezar <info@dvestezar.cz>
2025-04-21 15:37:48 +0200

    generování port info

--- libs/app/menus/app_node_red/menu_sel_node_instance.py
+++ libs/app/menus/app_node_red/menu_sel_node_instance.py
@@ -9,6 +9,7 @@ from libs.app.c_service_node import c_service_node
 from libs.app.install_instance import install_node_instance
 from .nd_menu import nd_menu
 from libs.app.c_cfg import cfg_data
+from libs.app.instanceHelper import ports,createPortInUseJson
 
 class menuEdit_select_nodeInstance(nd_menu):
     sysUsers:list[int,str]=[]
@@ -19,11 +20,19 @@ class menuEdit_select_nodeInstance(nd_menu):
         self._setAppHeader(TXT_MENUSEL_TIT)
         
         self.menu=[]
+        p=[]
 
         i=[]
+        lst_json=[] # obsahuje pole polí [ [<int_port>,<name_instance>],... ]
         for item in getSysUsers():
             # service = c_service_node(item[1])
             d=cfg_data(item[1])
+            p.append(int(d.port))
+            lst_json.append([
+                int(d.port),
+                item[1],
+                d.service.fullStatus()
+            ])
             i.append(
                 c_menu_item(
                     item[1],
@@ -34,6 +43,8 @@ class menuEdit_select_nodeInstance(nd_menu):
                     atRight=d.service.fullStatus() + f" | {TXT_PORT}: "+str(d.port)
                 )
             )
+        ports(p)
+        createPortInUseJson(lst_json,self.sslStatus)
         if i:
             self.menu.append(c_menu_title_label(TXT_TITLE_INSTANCE))
             self.menu.extend(i)
