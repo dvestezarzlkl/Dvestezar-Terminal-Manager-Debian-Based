7fec3cb02b507f3ce67e78fc22f756c8c67156c7
Jan Zedník <zednik@zlkl.cz>
2025-12-23 10:54:28 +0100

    přejmenování app dir pro řazení

--- libs/app/menus/app_node_red/nd_menu.py
+++
@@ -1,8 +0,0 @@
-from .lng.default import * 
-from libs.JBLibs.helper import loadLng
-loadLng()
-
-from libs.app.appHelper import menu as menuX
-
-class nd_menu(menuX):
-    appTitle:str = TXT_MAIN_NAME
