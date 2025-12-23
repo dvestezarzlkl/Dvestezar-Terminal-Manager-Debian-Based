045882e8e569d457323a1bc3ee265ad25e6b5d72
Jan Zedník <zednik@zlkl.cz>
2025-12-23 10:56:18 +0100

    přesun var

--- libs/app/g_def.py
+++ libs/app/g_def.py
@@ -1,6 +1,6 @@
-# tady jsou čisté definice pro vše, žádné výpočty apod.
+# tady jsou čisté definice pro vše, žádné výpočty, fn, class apod. !!
 
 CONFIG_NAME: str        = "config.ini"                 # jméno konfiguračního ini souboru
 CONFIG_ETC:  bool       = True                         # pokud je True, config.ini se hledá v /etc/jb_sys_apps/, jinak v cwd
-APP_NAME:   str        = "jb_sys_apps"                # jméno aplikace, pro složku v /etc/ a logy
-
+APP_NAME:    str        = "jb_sys_apps"                # jméno aplikace, pro složku v /etc/ a logy
+DISK_CFG:    str        = "settings.conf"              # jméno disk config souboru
